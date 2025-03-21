7b3c0306053601784a51c3d5e6665b6b25ed4852
harissonola <olaitanharisson@gmail.com>
2025-03-21 01:07:47 +0100

    Sort shops by name in DashboardController and update product description in shop template

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -110,6 +111,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $googleId = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $reward = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $lastMiningTime = null;
 
     public function __construct()
     {
@@ -455,6 +462,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setGoogleId(?string $googleId): static
     {
         $this->googleId = $googleId;
+
+        return $this;
+    }
+
+    public function getReward(): ?float
+    {
+        return $this->reward;
+    }
+
+    public function setReward(?float $reward): static
+    {
+        $this->reward = $reward;
+
+        return $this;
+    }
+
+    public function getLastMiningTime(): ?\DateTimeInterface
+    {
+        return $this->lastMiningTime;
+    }
+
+    public function setLastMiningTime(?\DateTimeInterface $lastMiningTime): static
+    {
+        $this->lastMiningTime = $lastMiningTime;
 
         return $this;
     }

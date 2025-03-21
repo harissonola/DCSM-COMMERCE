4d57043dfe78d6c6737cc1ad483bc1e927bf06a8
harissonola <olaitanharisson@gmail.com>
2025-03-21 01:14:37 +0100

    Update template path in ProductsController for consistency

--- src/Repository/ProductPriceRepository.php
+++ src/Repository/ProductPriceRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Product;
 use App\Entity\ProductPrice;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,6 +15,17 @@ class ProductPriceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, ProductPrice::class);
+    }
+
+    public function findLatestPrice(Product $product): ?ProductPrice
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.product = :product')
+            ->setParameter('product', $product)
+            ->orderBy('p.timestamp', 'DESC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
     //    /**

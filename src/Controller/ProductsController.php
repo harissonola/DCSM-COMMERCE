d469a31015317f68756f3d25b2f3934d4f7e9a6b
harissonola <olaitanharisson@gmail.com>
2025-03-28 02:23:02 +0100

    Rename route for selling products to follow naming conventions

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -198,7 +198,7 @@ class ProductsController extends AbstractController
     }
 
     // Méthodes existantes inchangées
-    #[Route('/sell-product/{slug}', name: 'sell_product', methods: ['POST'])]
+    #[Route('/sell-product/{slug}', name: 'app_sell_product', methods: ['POST'])]
     public function sellProduct(
         Request $request,
         ProductRepository $productRepository,

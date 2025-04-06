548f6c5883fb1f94a8657dbb89df2ee8562dff49
harissonola <olaitanharisson@gmail.com>
2025-04-06 22:36:07 +0100

    Fix: Use getPrice() instead of getProductPrice() to retrieve product price
    This commit fixes an issue where the `getProductPrice()` method was being used to retrieve the product price in the `removeProduct` action of the `UserController`. This method has been replaced with `getPrice()` to ensure that the correct product price is retrieved for the user's balance update.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -31,7 +31,7 @@ class UserController extends AbstractController
         }
 
         // Récupérer le prix actuel du produit
-        $productPrice = $product->getProductPrice();
+        $productPrice = $product->getPrice();
 
         // Ajouter le prix au solde de l'utilisateur (remboursement)
         $user->setBalance($user->getBalance() + $productPrice);

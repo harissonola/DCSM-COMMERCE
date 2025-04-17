d15f8fe64b11a1b4119038f1770acc1b6ca15440
harissonola <olaitanharisson@gmail.com>
2025-04-17 20:48:24 +0100

    fix(legal): update contact email addresses
    - Update email addresses in conditions and confidentiality pages to bictrary@alwaysdata.net.
    - Reflects the correct contact information for user inquiries and data requests.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
